f36d9945e78ef0e9d9a0a83138eee56cce5e3e70
JuggernautTej <tiwatej@gmail.com>
2024-10-02 19:23:05 +0100

    attempted task 6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -100,5 +100,58 @@ class FilesController{
             parentId,
         });
     }
+    static async getShow(req, res) {
+        const token = req.header('X-Token');
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        const key = `auth_${token}`;
+        const userId = await redisClient.get(key);
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        const fileId = req.params.id;
+        try {
+            const file = await dbClient.db.collection('files').findOne({
+                _id: ObjectId(fileId),
+                userId: ObjectId(userId),
+            });
+            if (!file) {
+                return res.status(404).json({ error: 'Not found' });
+            }
+            return res.status(200).json(file);
+        } catch (error) {
+            return res.status(404).json({ error: 'Not found' });
+        }
+    }
+
+    static async getIndex(req, res) {
+        const token = req.header('X-Token');
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        const key = `auth_${token}`;
+        const userId = await redisClient.get(key);
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        const { parentId = '0', page = 0 } = req.query;
+        const pageSize = 20;
+        const skip = pageSize * parseInt(page, 10);
+        let filter = { userId: ObjectId(userId) };
+        if (parentId !== '0') {
+            filter.parentId = ObjectId(parentId);
+        }
+        try {
+            const files = await dbClient.db.collection('files')
+            .find(filter)
+            .skip(skip)
+            .limit(pageSize)
+            .toArray();
+            return res.status(200).json(files);
+        } catch (error) {
+            return res.status(500).json({ error: 'Error retrieving files' });
+        }
+    }
 }
 module.exports = FilesController;
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,8 @@ router.get('/stats',  AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 router.post('/files', FilesController.postUpload);
 router.post('/users', UsersController.postNew);
