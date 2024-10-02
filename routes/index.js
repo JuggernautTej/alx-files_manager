44c30c1b451ebc079fdd78078de066dcba33a8ae
Emmanuel <kcemmy03@gmail.com>
2024-10-02 23:49:57 +0100

    Added more routes to the file

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,8 @@ router.get('/stats',  AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id', FilesController.getShow);
 
 router.post('/files', FilesController.postUpload);
 router.post('/users', UsersController.postNew);
